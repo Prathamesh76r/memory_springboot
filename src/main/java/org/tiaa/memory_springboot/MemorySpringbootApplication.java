2ff6d10f01dc5c2276db72e7a56151d5377567ad
Prathamesh76r <prathamesh76r@gmail.com>
2021-07-29 17:22:14 +0530

    Add files via upload

---
+++ src/main/java/org/tiaa/memory_springboot/MemorySpringbootApplication.java
@@ -0,0 +1,13 @@
+package org.tiaa.memory_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemorySpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemorySpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/tiaa/memory_springboot/MemorySpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tiaa.memory_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemorySpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
